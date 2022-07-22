d716d39053e65880a8bcfd2e3efeb7dbd707b12b
rabilongo <rabi@LAPTOP-RABI.localdomain>
2022-07-22 10:27:21 +0200

    clean

--- hello-world/target/debug/deps/hello_world-5ef53d840774e26e.d
+++
@@ -1,5 +0,0 @@
-/home/rabi/documents/rust/hello-world/target/debug/deps/hello_world-5ef53d840774e26e: src/main.rs
-
-/home/rabi/documents/rust/hello-world/target/debug/deps/hello_world-5ef53d840774e26e.d: src/main.rs
-
-src/main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/home/rabi/documents/rust/hello-world/target/debug/hello-world: /home/rabi/documents/rust/hello-world/src/main.rs
